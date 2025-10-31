2c916794b8a465b64562b330404be24c4cb49f5d
Akash Bhattacharya <akashbhattacharyak1314@gmail.com>
2025-10-31 22:29:32 +0530

    Dom Forms and inputs

--- DOM/script.js
+++ DOM/script.js
@@ -1,8 +1,22 @@
 const title = document.querySelector(".title")
 const button = document.querySelector(".button")
+const inp = document.querySelector("input")
+// .input ❌ because I have not decleared that as a class/id
+const dropDown = document.querySelector(".drop-down")
 
 button.addEventListener("click",()=>{
-    title.textContent = "Bye Bye Universe from js"
-    title.style.color = "red"
-    title.style.fontSize = "2rem"
-}) 
+    cosmaticChanges(s = "Changes from function",c = "blue",f ="2rem")
+})
+
+inp.addEventListener("input",function(data){
+    console.log(data);
+    
+})
+dropDown.addEventListener("change",(company)=>{
+    title.textContent = ` Welcome to ${company.target.value} Akku`
+})
+function cosmaticChanges(s,c,f){
+    title.textContent = s
+    title.style.color = c
+    title.style.fontSize = f
+} 
