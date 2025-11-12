7a7638435aecc0a91fae40f068aef476654691ce
Akash Bhattacharya <akashbhattacharyak1314@gmail.com>
2025-11-12 22:46:43 +0530

    Regex form validation

---
+++ FormValidations/main.js
@@ -0,0 +1,10 @@
+const username =  document.querySelector("#username")
+const email =  document.querySelector("#email")
+const form = document.querySelector("form")
+const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
+const usernameRegex = /^[a-zA-Z0-9]+$/
+form.addEventListener("submit",(dets)=>{
+    dets.preventDefault()
+    
+     
+})
