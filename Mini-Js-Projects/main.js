58e53793f813b6c7d97d95c4313a9efa7f546c58
Akash Bhattacharya <akashbhattacharyak1314@gmail.com>
2025-11-19 18:44:07 +0530

    Form validation updated

--- Mini-Js-Projects/main.js
+++ Mini-Js-Projects/main.js
@@ -1,25 +1,37 @@
 const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
-const passwordPattern = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$/
+const passwordPattern =
+    /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
 let email = document.querySelector("#email")
 let password = document.querySelector("#password")
 let form = document.querySelector(".card-content")
 let emailError = document.querySelector("#emailError")
 let passwordError = document.querySelector("#passwordError")
-let emailans = emailPattern.test(email.value)
-let passwordans = passwordPattern.test(password.value)
+
 
 form.addEventListener("submit", (dets) => {
     dets.preventDefault()
     let valid = true
+    let emailans = emailPattern.test(email.value.trim())
+    let passwordans = passwordPattern.test(password.value.trim())
     if (!emailans) {
         emailError.textContent = "Invalid Email"
-        document.querySelector(".error").style.display = "initial"
+        emailError.style.display = "initial"
         valid = false
-    } else{
+    } else {
         emailError.textContent = ""
+        emailError.style.display = "none";
     }
     if (!passwordans) {
         passwordError.textContent = "Invalid passowrd"
+        passwordError.style.display = "initial"
         valid = false
+    } else {
+        passwordError.textContent = ""
+        passwordError.style.display = "none";
+    }
+    if (valid) {
+        alert("Form submitted successfully!")
+        form.reset()
     }
 })
+//DOM → Tailwind → API mini apps → JS deep concepts → React/Next.js → Projects → Job
